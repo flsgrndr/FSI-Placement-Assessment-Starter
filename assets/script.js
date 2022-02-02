61c5f45a88a8955ba998e0ea9a3008e3d6ef22db
unknown <jam319@protonmail.com>
2022-02-01 19:37:40 -0500

    removed old instructional commenting

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,10 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-
 let yourName = "Jason Marshall"
 
-// We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
+let gb = 0
+let cc = 0
+let sugar = 0
 
-// selecting the element with an id of credit
 let credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
 let gbPlusBtn = document.querySelector('#add-gb')
 let gbMinusBtn = document.querySelector('#minus-gb')
 let ccPlusBtn = document.querySelector('#add-cc')
@@ -46,10 +40,8 @@ function updateTotalQuantity(totalQuantity){
 
 }
 
-// Code to update name display
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
 gbPlusBtn.addEventListener('click', function() {
 
     console.log('Gingerbread + button was clicked!')
@@ -115,5 +107,3 @@ sugarMinusBtn.addEventListener('click', function() {
     updateTotalQuantity(totalQuantity)
 
 })
-
-// TODO: Hook up event listeners for the rest of the buttons
